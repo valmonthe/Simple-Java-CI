e0a318cd82d4b6757d1de89650b13ff9b9e73a91
Valery M. <projets.monthe@gmail.com>
2025-05-06 19:16:47 +0100

    vérification du CI

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-    public static int add(int a, int b) { return a + b; }
+    public static int add(int a, int b) { return a + b+1; }
     public static int subtract(int a, int b) { return a - b; }
     public static int multiply(int a, int b) { return a * b; }
     public static double divide(int a, int b) {
